f8d6348ed1b326cbf6721dd57413ea82960dfdb0
Lor-ran <thoughtsmaster@outlook.com>
2020-07-05 20:34:33 +0800

    父子窗体与新功能页面
    暂无数据库，跳过了登录界面，绝大部分按钮因无数据库连接点击会出错，等待连接数据库，新功能建立了页面（在做了，新建文件夹），未具体实现，也许会有个别功能疏漏了（应该没有吧），发现了再修改

--- c#体育馆预约/CS/用户界面.cs
+++ c#体育馆预约/CS/用户界面.cs
@@ -74,8 +74,17 @@ namespace WindowsApp1
 		public void Form1_Load(object sender, EventArgs e)
 		{
 			this.WindowState = FormWindowState.Maximized;
+            funcpanel.Location = new Point(MenuStrip1.Location.X, MenuStrip1.Location.Y + MenuStrip1.Height + 3);       
+            funcpanel.Width = 200;
+            //获取屏幕高度、设置左侧panel的高度
+            funcpanel.Height = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height - MenuStrip1.Location.Y - MenuStrip1.Height - 3;
+            Settingpanel.Location = new Point(funcpanel.Location.X + funcpanel.Width, funcpanel.Location.Y);
+            Settingpanel.Height = 100;
+            Settingpanel.Width = this.Width - 200;
+            this.IsMdiContainer = true;
 		}
 		
+
 		public void tsmiUserPwd_Click(object sender, EventArgs e)
 		{
 			修改密码.Default.ShowDialog();
@@ -90,8 +99,125 @@ namespace WindowsApp1
 		{
 			Yuyue.Default.Show();
 		}
-		
-		
-	}
+
+        private void functab_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void showChildForm(Form childform)
+        {
+            //添加子窗体函数
+            Form form = childform;
+            form.MdiParent = this;
+            //设置为子窗体
+            form.ControlBox = false;
+            //去除控制栏
+            form.FormBorderStyle = FormBorderStyle.None;
+            //去除边框
+            form.Width = this.Width - 200;
+            form.Height = SystemInformation.WorkingArea.Height - funcpanel.Location.Y - Settingpanel.Height - 6;
+            form.Show();
+            form.Location = new Point(200, 103);
+        }
+
+        private void reserve_Click(object sender, EventArgs e)
+        {
+            //预约场地
+            Form form = new Yuyue();
+            //要添加的子窗体
+            showChildForm(form);
+            //添加
+        }
+
+        private void equipmentRent_Click(object sender, EventArgs e)
+        {
+            //器材租借
+            Form form = new 器材租借();
+            showChildForm(form);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void equipmentMg_Click(object sender, EventArgs e)
+        {
+            //器材管理
+            Form form = new 器材管理();
+            showChildForm(form);
+        }
+
+        private void groundTypeSearch_Click(object sender, EventArgs e)
+        {
+            //查询场地类型
+            Form form = new 查询场地类型();
+            showChildForm(form);
+        }
+
+        private void groundInfoSearch_Click(object sender, EventArgs e)
+        {
+            //查询场地信息
+            Form form = new 查询场地信息();
+            showChildForm(form);
+        }
+
+        private void lentInfoSearch_Click(object sender, EventArgs e)
+        {
+            //查询租借情况
+            Form form = new 查询场地租借情况();
+            showChildForm(form);
+        }
+
+        private void addGroundType_Click(object sender, EventArgs e)
+        {
+            //添加场地类型
+            Form form = new 添加场地类型();
+            showChildForm(form);
+        }
+
+        private void addGroundNum_Click(object sender, EventArgs e)
+        {
+            //添加场地数量
+            Form form = new 添加场地信息();
+            showChildForm(form);
+        }
+
+        private void userMg_Click(object sender, EventArgs e)
+        {
+            //用户管理，增删用户
+            Form form = new 用户管理();
+            showChildForm(form);
+        }
+
+        private void orderSearch_Click(object sender, EventArgs e)
+        {
+            //查询用户订单
+            Form form = new 订单查询();
+            showChildForm(form);
+        }
+
+        private void updatePass_Click(object sender, EventArgs e)
+        {
+            //修改用户密码
+            Form form = new 修改密码();
+            showChildForm(form);
+        }
+
+        private void incomeStatistics_Click(object sender, EventArgs e)
+        {
+            //收入统计
+            Form form = new 收入统计();
+            showChildForm(form);
+        }
+
+        private void bulletin_Click(object sender, EventArgs e)
+        {
+            //发布公告
+            Form form = new 发布公告();
+            showChildForm(form);
+        }
+    }
 	
 }
--- c#体育馆预约/CS/用户界面.designer.cs
+++ c#体育馆预约/CS/用户界面.designer.cs
@@ -46,90 +46,269 @@ namespace WindowsApp1
 		[System.Diagnostics.DebuggerStepThrough()]
 			private void InitializeComponent()
 			{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(用户界面));
-			this.MenuStrip1 = new System.Windows.Forms.MenuStrip();
-			base.Load += new System.EventHandler(Form1_Load);
-			this.tsmiUser = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmiUserPwd = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmiUserPwd.Click += new System.EventHandler(this.tsmiUserPwd_Click);
-			this.tsmiUserOrders = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmiUserOrders.Click += new System.EventHandler(this.tsmiUserOrders_Click);
-			this.tsmiAppointment = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmiAppointment.Click += new System.EventHandler(this.tsmiAppointment_Click);
-			this.帮助tsmiHelp = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmiClose = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmiCancellation = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmiCancellation.Click += new System.EventHandler(this.tsmiCancellation_Click);
-			this.tsmiClose1 = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmiClose1.Click += new System.EventHandler(this.tsmiClose1_Click);
-			this.MenuStrip1.SuspendLayout();
-			this.SuspendLayout();
-			//
-			//MenuStrip1
-			//
-			this.MenuStrip1.BackColor = System.Drawing.Color.Transparent;
-			resources.ApplyResources(this.MenuStrip1, "MenuStrip1");
-			this.MenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-			this.MenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.tsmiUser, this.tsmiAppointment, this.帮助tsmiHelp, this.tsmiClose});
-			this.MenuStrip1.Name = "MenuStrip1";
-			//
-			//tsmiUser
-			//
-			this.tsmiUser.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.tsmiUserPwd, this.tsmiUserOrders});
-			this.tsmiUser.Name = "tsmiUser";
-			resources.ApplyResources(this.tsmiUser, "tsmiUser");
-			//
-			//tsmiUserPwd
-			//
-			this.tsmiUserPwd.Name = "tsmiUserPwd";
-			resources.ApplyResources(this.tsmiUserPwd, "tsmiUserPwd");
-			//
-			//tsmiUserOrders
-			//
-			this.tsmiUserOrders.Name = "tsmiUserOrders";
-			resources.ApplyResources(this.tsmiUserOrders, "tsmiUserOrders");
-			//
-			//tsmiAppointment
-			//
-			this.tsmiAppointment.Name = "tsmiAppointment";
-			resources.ApplyResources(this.tsmiAppointment, "tsmiAppointment");
-			//
-			//帮助tsmiHelp
-			//
-			this.帮助tsmiHelp.ForeColor = System.Drawing.Color.Black;
-			this.帮助tsmiHelp.Name = "帮助tsmiHelp";
-			resources.ApplyResources(this.帮助tsmiHelp, "帮助tsmiHelp");
-			//
-			//tsmiClose
-			//
-			this.tsmiClose.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.tsmiCancellation, this.tsmiClose1});
-			this.tsmiClose.Name = "tsmiClose";
-			resources.ApplyResources(this.tsmiClose, "tsmiClose");
-			//
-			//tsmiCancellation
-			//
-			this.tsmiCancellation.Name = "tsmiCancellation";
-			resources.ApplyResources(this.tsmiCancellation, "tsmiCancellation");
-			//
-			//tsmiClose1
-			//
-			this.tsmiClose1.Name = "tsmiClose1";
-			resources.ApplyResources(this.tsmiClose1, "tsmiClose1");
-			//
-			//用户界面
-			//
-			resources.ApplyResources(this, "$this");
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackgroundImage = global::My.Resources.Resources.捕获1;
-			this.Controls.Add(this.MenuStrip1);
-			this.MainMenuStrip = this.MenuStrip1;
-			this.MaximizeBox = false;
-			this.Name = "用户界面";
-			this.MenuStrip1.ResumeLayout(false);
-			this.MenuStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
-			
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(用户界面));
+            this.MenuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiUser = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiUserPwd = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiUserOrders = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAppointment = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助tsmiHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCancellation = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiClose1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.funcpanel = new System.Windows.Forms.Panel();
+            this.reserve = new System.Windows.Forms.Button();
+            this.Settingpanel = new System.Windows.Forms.Panel();
+            this.equipmentMg = new System.Windows.Forms.Button();
+            this.equipmentRent = new System.Windows.Forms.Button();
+            this.changguanDataSet61 = new WindowsApp1.changguanDataSet6();
+            this.groundTypeSearch = new System.Windows.Forms.Button();
+            this.userMg = new System.Windows.Forms.Button();
+            this.orderSearch = new System.Windows.Forms.Button();
+            this.incomeStatistics = new System.Windows.Forms.Button();
+            this.bulletin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groundInfoSearch = new System.Windows.Forms.Button();
+            this.lentInfoSearch = new System.Windows.Forms.Button();
+            this.addGroundType = new System.Windows.Forms.Button();
+            this.addGroundNum = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.updatePass = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MenuStrip1.SuspendLayout();
+            this.funcpanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.changguanDataSet61)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MenuStrip1
+            // 
+            this.MenuStrip1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.MenuStrip1, "MenuStrip1");
+            this.MenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiUser,
+            this.tsmiAppointment,
+            this.帮助tsmiHelp,
+            this.tsmiClose});
+            this.MenuStrip1.Name = "MenuStrip1";
+            // 
+            // tsmiUser
+            // 
+            this.tsmiUser.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiUserPwd,
+            this.tsmiUserOrders});
+            this.tsmiUser.Name = "tsmiUser";
+            resources.ApplyResources(this.tsmiUser, "tsmiUser");
+            // 
+            // tsmiUserPwd
+            // 
+            this.tsmiUserPwd.Name = "tsmiUserPwd";
+            resources.ApplyResources(this.tsmiUserPwd, "tsmiUserPwd");
+            this.tsmiUserPwd.Click += new System.EventHandler(this.tsmiUserPwd_Click);
+            // 
+            // tsmiUserOrders
+            // 
+            this.tsmiUserOrders.Name = "tsmiUserOrders";
+            resources.ApplyResources(this.tsmiUserOrders, "tsmiUserOrders");
+            this.tsmiUserOrders.Click += new System.EventHandler(this.tsmiUserOrders_Click);
+            // 
+            // tsmiAppointment
+            // 
+            this.tsmiAppointment.Name = "tsmiAppointment";
+            resources.ApplyResources(this.tsmiAppointment, "tsmiAppointment");
+            this.tsmiAppointment.Click += new System.EventHandler(this.tsmiAppointment_Click);
+            // 
+            // 帮助tsmiHelp
+            // 
+            this.帮助tsmiHelp.ForeColor = System.Drawing.Color.Black;
+            this.帮助tsmiHelp.Name = "帮助tsmiHelp";
+            resources.ApplyResources(this.帮助tsmiHelp, "帮助tsmiHelp");
+            // 
+            // tsmiClose
+            // 
+            this.tsmiClose.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiCancellation,
+            this.tsmiClose1});
+            this.tsmiClose.Name = "tsmiClose";
+            resources.ApplyResources(this.tsmiClose, "tsmiClose");
+            // 
+            // tsmiCancellation
+            // 
+            this.tsmiCancellation.Name = "tsmiCancellation";
+            resources.ApplyResources(this.tsmiCancellation, "tsmiCancellation");
+            this.tsmiCancellation.Click += new System.EventHandler(this.tsmiCancellation_Click);
+            // 
+            // tsmiClose1
+            // 
+            this.tsmiClose1.Name = "tsmiClose1";
+            resources.ApplyResources(this.tsmiClose1, "tsmiClose1");
+            this.tsmiClose1.Click += new System.EventHandler(this.tsmiClose1_Click);
+            // 
+            // funcpanel
+            // 
+            this.funcpanel.Controls.Add(this.label4);
+            this.funcpanel.Controls.Add(this.label3);
+            this.funcpanel.Controls.Add(this.updatePass);
+            this.funcpanel.Controls.Add(this.label2);
+            this.funcpanel.Controls.Add(this.addGroundNum);
+            this.funcpanel.Controls.Add(this.addGroundType);
+            this.funcpanel.Controls.Add(this.lentInfoSearch);
+            this.funcpanel.Controls.Add(this.groundInfoSearch);
+            this.funcpanel.Controls.Add(this.label1);
+            this.funcpanel.Controls.Add(this.bulletin);
+            this.funcpanel.Controls.Add(this.incomeStatistics);
+            this.funcpanel.Controls.Add(this.orderSearch);
+            this.funcpanel.Controls.Add(this.userMg);
+            this.funcpanel.Controls.Add(this.groundTypeSearch);
+            this.funcpanel.Controls.Add(this.equipmentMg);
+            this.funcpanel.Controls.Add(this.reserve);
+            this.funcpanel.Controls.Add(this.equipmentRent);
+            resources.ApplyResources(this.funcpanel, "funcpanel");
+            this.funcpanel.Name = "funcpanel";
+            // 
+            // reserve
+            // 
+            resources.ApplyResources(this.reserve, "reserve");
+            this.reserve.Name = "reserve";
+            this.reserve.UseVisualStyleBackColor = true;
+            this.reserve.Click += new System.EventHandler(this.reserve_Click);
+            // 
+            // Settingpanel
+            // 
+            resources.ApplyResources(this.Settingpanel, "Settingpanel");
+            this.Settingpanel.Name = "Settingpanel";
+            // 
+            // equipmentMg
+            // 
+            resources.ApplyResources(this.equipmentMg, "equipmentMg");
+            this.equipmentMg.Name = "equipmentMg";
+            this.equipmentMg.UseVisualStyleBackColor = true;
+            this.equipmentMg.Click += new System.EventHandler(this.equipmentMg_Click);
+            // 
+            // equipmentRent
+            // 
+            resources.ApplyResources(this.equipmentRent, "equipmentRent");
+            this.equipmentRent.Name = "equipmentRent";
+            this.equipmentRent.UseVisualStyleBackColor = true;
+            this.equipmentRent.Click += new System.EventHandler(this.equipmentRent_Click);
+            // 
+            // changguanDataSet61
+            // 
+            this.changguanDataSet61.DataSetName = "changguanDataSet6";
+            this.changguanDataSet61.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // groundTypeSearch
+            // 
+            resources.ApplyResources(this.groundTypeSearch, "groundTypeSearch");
+            this.groundTypeSearch.Name = "groundTypeSearch";
+            this.groundTypeSearch.UseVisualStyleBackColor = true;
+            this.groundTypeSearch.Click += new System.EventHandler(this.groundTypeSearch_Click);
+            // 
+            // userMg
+            // 
+            resources.ApplyResources(this.userMg, "userMg");
+            this.userMg.Name = "userMg";
+            this.userMg.UseVisualStyleBackColor = true;
+            this.userMg.Click += new System.EventHandler(this.userMg_Click);
+            // 
+            // orderSearch
+            // 
+            resources.ApplyResources(this.orderSearch, "orderSearch");
+            this.orderSearch.Name = "orderSearch";
+            this.orderSearch.UseVisualStyleBackColor = true;
+            this.orderSearch.Click += new System.EventHandler(this.orderSearch_Click);
+            // 
+            // incomeStatistics
+            // 
+            resources.ApplyResources(this.incomeStatistics, "incomeStatistics");
+            this.incomeStatistics.Name = "incomeStatistics";
+            this.incomeStatistics.UseVisualStyleBackColor = true;
+            this.incomeStatistics.Click += new System.EventHandler(this.incomeStatistics_Click);
+            // 
+            // bulletin
+            // 
+            resources.ApplyResources(this.bulletin, "bulletin");
+            this.bulletin.Name = "bulletin";
+            this.bulletin.UseVisualStyleBackColor = true;
+            this.bulletin.Click += new System.EventHandler(this.bulletin_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // groundInfoSearch
+            // 
+            resources.ApplyResources(this.groundInfoSearch, "groundInfoSearch");
+            this.groundInfoSearch.Name = "groundInfoSearch";
+            this.groundInfoSearch.UseVisualStyleBackColor = true;
+            this.groundInfoSearch.Click += new System.EventHandler(this.groundInfoSearch_Click);
+            // 
+            // lentInfoSearch
+            // 
+            resources.ApplyResources(this.lentInfoSearch, "lentInfoSearch");
+            this.lentInfoSearch.Name = "lentInfoSearch";
+            this.lentInfoSearch.UseVisualStyleBackColor = true;
+            this.lentInfoSearch.Click += new System.EventHandler(this.lentInfoSearch_Click);
+            // 
+            // addGroundType
+            // 
+            resources.ApplyResources(this.addGroundType, "addGroundType");
+            this.addGroundType.Name = "addGroundType";
+            this.addGroundType.UseVisualStyleBackColor = true;
+            this.addGroundType.Click += new System.EventHandler(this.addGroundType_Click);
+            // 
+            // addGroundNum
+            // 
+            resources.ApplyResources(this.addGroundNum, "addGroundNum");
+            this.addGroundNum.Name = "addGroundNum";
+            this.addGroundNum.UseVisualStyleBackColor = true;
+            this.addGroundNum.Click += new System.EventHandler(this.addGroundNum_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // updatePass
+            // 
+            resources.ApplyResources(this.updatePass, "updatePass");
+            this.updatePass.Name = "updatePass";
+            this.updatePass.UseVisualStyleBackColor = true;
+            this.updatePass.Click += new System.EventHandler(this.updatePass_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // 用户界面
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::My.Resources.Resources.捕获1;
+            this.Controls.Add(this.Settingpanel);
+            this.Controls.Add(this.funcpanel);
+            this.Controls.Add(this.MenuStrip1);
+            this.MainMenuStrip = this.MenuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "用户界面";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MenuStrip1.ResumeLayout(false);
+            this.MenuStrip1.PerformLayout();
+            this.funcpanel.ResumeLayout(false);
+            this.funcpanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.changguanDataSet61)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 		
 		internal MenuStrip MenuStrip1;
@@ -141,6 +320,26 @@ namespace WindowsApp1
 		internal ToolStripMenuItem tsmiCancellation;
 		internal ToolStripMenuItem tsmiClose1;
 		internal ToolStripMenuItem tsmiUserOrders;
-	}
+        private Panel funcpanel;
+        private Panel Settingpanel;
+        private Button reserve;
+        private Button equipmentMg;
+        private Button equipmentRent;
+        private changguanDataSet6 changguanDataSet61;
+        private Button bulletin;
+        private Button incomeStatistics;
+        private Button orderSearch;
+        private Button userMg;
+        private Button groundTypeSearch;
+        private Label label2;
+        private Button addGroundNum;
+        private Button addGroundType;
+        private Button lentInfoSearch;
+        private Button groundInfoSearch;
+        private Label label1;
+        private Label label4;
+        private Label label3;
+        private Button updatePass;
+    }
 	
 }
